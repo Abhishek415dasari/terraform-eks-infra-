5105e01cfbaf33222816d0e9f7663e1abdcc5fd4
Abhishek415dasari <abhishek415dasari@gmail.com>
2025-07-07 23:51:57 -0500

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,29 @@
+output "cluster_endpoint" {
+  description = "EKS cluster API endpoint"
+  value       = aws_eks_cluster.this.endpoint
+}
+
+output "cluster_ca_data" {
+  description = "Base64-encoded CA certificate data"
+  value       = aws_eks_cluster.this.certificate_authority[0].data
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = module.networking.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = module.networking.private_subnet_ids
+}
+
+output "node_group_name" {
+  description = "Name of the managed node group"
+  value       = aws_eks_node_group.this.node_group_name
+}
+
+output "karpenter_role_arn" {
+  description = "IAM role ARN for Karpenter"
+  value       = module.karpenter.karpenter_role_arn
+}
