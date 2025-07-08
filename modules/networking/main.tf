9e7a78a4f3a5463342f0fd81f10d21962fe59580
Abhishek415dasari <abhishek415dasari@gmail.com>
2025-07-08 00:06:07 -0500

    main.tf

---
+++ modules/networking/main.tf
@@ -0,0 +1,60 @@
+resource "aws_vpc" "this" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_subnet" "public" {
+  for_each = toset(var.public_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value
+  availability_zone = var.azs[0]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+}
+
+resource "aws_eip" "nat" {
+  vpc = true
+}
+
+resource "aws_subnet" "private" {
+  for_each = toset(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value
+  availability_zone = var.azs[0]
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  for_each      = aws_subnet.public
+  subnet_id     = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  for_each      = aws_subnet.private
+  subnet_id     = each.value.id
+  route_table_id = aws_route_table.private.id
+}
